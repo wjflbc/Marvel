5ac538c681f105696863ff6a1a01591f953b2873
Your Name <ankalina2796@gmail.com>
2021-11-19 17:59:56 +0200

    Done CharList

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,17 +1,24 @@
 import {Component} from 'react'
 import MarvelService from "../../services/MarvelService";
+import Spinner from "../spiner/spiner";
+import ErrorMessage from "../errorMessage/ErrorMessage";
 
 import './charList.scss';
+
+
 
 
 
 class CharList extends Component {
 
     state = {
-        charList: []
+        charList: [],
+        loading: true,
+        error: false
     }
 
     marvelService = new MarvelService();
+
     componentDidMount() {
         this.updateCharList();
     }
@@ -25,35 +32,58 @@ class CharList extends Component {
 
     onCharListLoaded = (charList) => {
         this.setState({
-            charList
+            charList,
+            loading: false
         })
     }
 
+
+
     renderChars = (arr) => {
         return arr.map((item) => {
+            const {thumbnail, name, id} = item;
+            console.log(thumbnail)
+
+            let styleImg = {'objectFit' : 'cover'};
+
+            if (thumbnail ==='http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
+               styleImg = {'objectFit' : 'contain'};
+            }
             return (
                 <li className="char__item"
-                    key={item.id}>
-                    <img src={item.thumbnail} alt={item.name}/>
-                    <div className="char__name">{item.name}</div>
+                    key={id}>
+                    <img src={thumbnail} alt={name} style={styleImg}/>
+                    <div className="char__name">{name}</div>
                 </li>
             )
         })
     }
 
-
-
+    onError = () => {
+        this.setState({
+            loading: false,
+            error: true
+        })
+    }
 
     render() {
-        const {charList} = this.state;
-
-
+        const {charList, loading, error} = this.state;
         const charAll = this.renderChars(charList);
+
+        const spiner = loading ? <Spinner/> : null;
+        const errorMeessage = error ? <ErrorMessage/> : null;
+        const content = !(loading || error) ? charAll : null;
+
+
 
         return (
             <div className="char__list">
+                {spiner}
+                {errorMeessage}
+                {content}
                 <ul className="char__grid">
                     {charAll}
+
                     {/*<li className="char__item">*/}
                     {/*    <img src={abyss} alt="abyss"/>*/}
                     {/*    <div className="char__name">Abyss</div>*/}
